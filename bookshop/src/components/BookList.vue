911b4b0065b40192fe2b10975e1cc9f091774d3f
Pavel Avdyeyev <pavel.avdyeyev@opower.com>
2017-11-01 02:48:23 +0200

    to prev. Bootlist, Login

---
+++ bookshop/src/components/BookList.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <v-select
+      v-bind:items="genres"
+      v-model="genre"
+      label="Select Genre"
+      class="input-group--focused"
+      item-value="genre_id"
+      item-text="genre_name"
+    ></v-select>
+    <v-select
+      v-bind:items="authors"
+      v-model="author"
+      label="Select Author"
+      class="input-group--focused"
+      item-value="author_id"
+      item-text="author_name"
+    ></v-select>
+    <v-data-table :items="books"
+                  v-bind:headers="headers"
+                  hide-actions
+                  class="elevation-1">
+      <template slot="items" slot-scope="props">
+        <td class="text-xs-center">{{ props.item.book_name }}</td>
+        <td class="text-xs-center">{{ props.item.authors.map( q => q.author_name).join(', ') }}</td>
+        <td class="text-xs-center">{{ props.item.genres.map( q => q.genre_name).join(', ') }}</td>
+        <td class="text-xs-center">{{ props.item.book_description }}</td>
+        <td class="text-xs-center">{{ props.item.book_price }}</td>
+        <td><router-link :to="{ name: 'book', params: {id: props.item.book_id} }" class="nav-item nav-link">Details</router-link></td>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'booklist',
+    data () {
+      return {
+        headers: [
+          { text: 'Book', value: 'book_name', align: 'center' },
+          { text: 'Author', value: 'authors', align: 'center' },
+          { text: 'Genre', value: 'genres', align: 'center' },
+          { text: 'Description', value: 'book_description', align: 'center', sortable: false },
+          { text: 'Price', value: 'book_price', align: 'center' }
+        ],
+        genre: null,
+        author: null
+      }
+    },
+    computed: {
+      books () {
+        let books = this.$store.getters.books
+        if (this.genre) {
+          books = books.filter(book => book.genres.find(genre => genre.genre_id === this.genre))
+        }
+        if (this.author) {
+          books = books.filter(book => book.authors.find(author => author.author_id === this.author))
+        }
+        return books
+      },
+      genres () {
+        return this.$store.getters.genres
+      },
+      authors () {
+        return this.$store.getters.authors
+      }
+    },
+    mounted () {
+      if (this.books.length === 0) {
+        this.$store.dispatch('booklist')
+      }
+      if (this.genres.length === 0) {
+        this.$store.dispatch('genres')
+      }
+      if (this.authors.length === 0) {
+        this.$store.dispatch('authors')
+      }
+    }
+  }
+</script>
---
+++ bookshop/src/components/Login.vue
@@ -0,0 +1,82 @@
+<template>
+    <div id="login">
+
+      <div v-if="!isLoggedIn" class="form-group">
+
+        <v-form v-model="valid" ref="form" lazy-validation>
+
+          <v-text-field
+            label="E-mail"
+            v-model="email"
+            :rules="emailRules"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            :rules="passwordRules"
+            :counter="10"
+            required
+          ></v-text-field>
+
+          <v-btn
+            @click="singin"
+            :disabled="!valid"
+          >
+            submit
+          </v-btn>
+          <v-btn @click="clear">clear</v-btn>
+
+        </v-form>
+        <router-link :to="{ name: 'registration' }">Registration</router-link>
+
+      </div>
+      <div v-else>
+        Logged in
+        <v-btn @click="logOut" >Log Out</v-btn>
+      </div>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data () {
+      return {
+        valid: false,
+        password: '',
+        passwordRules: [
+          (v) => !!v || 'Password is required',
+          (v) => v.length >= 10 || 'Passsword must be more than 10 characters'
+        ],
+        email: '',
+        emailRules: [
+          (v) => !!v || 'E-mail is required',
+          (v) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+        ]
+      }
+    },
+    computed: {
+      isLoggedIn () {
+        return this.$store.getters.isLoggedIn
+      }
+    },
+    methods: {
+      singin () {
+        if (this.$refs.form.validate()) {
+          this.$store.dispatch('login', {login: this.name, password: this.password})
+        }
+      },
+      logOut () {
+        this.$store.dispatch('logout')
+      },
+      clear () {
+        this.$refs.form.reset()
+      }
+    }
+  }
+</script>
+
