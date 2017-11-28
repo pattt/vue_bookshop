4442b94b3c252b0c218b50e8b901a47fbd316558
alex <agubarev@spotoption.com>
2017-11-28 17:30:46 +0200

    removed configs from src

--- bookshop/src/config/dev.env.js
+++
@@ -1,6 +0,0 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  api_url: 'http://autoshop.local:8081/api/'
-})
--- bookshop/src/config/prod.env.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  api_url: "'http://autoshop.local:8081/api/'"
-}
